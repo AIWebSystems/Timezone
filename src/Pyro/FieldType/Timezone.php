ac388651a96a3638e13dca04c7ff91923f802f96
Matthew Klahorst <mklahorst@gmail.com>
2014-02-17 12:36:21 -0800

    Create an opt group on the drop down to make it easier to find.
    We need to make a better drop down for that with Opt group, timezone abbreviations, offsets in the label, broken down by Region.

--- src/Pyro/FieldType/Timezone.php
+++ src/Pyro/FieldType/Timezone.php
@@ -43,11 +43,24 @@ class Timezone extends FieldTypeAbstract
             $choices[null] = '---';
         }
 
-        foreach (timezone_identifiers_list() as $key => $val)
+        
+        $zones = timezone_identifiers_list();
+        
+        foreach ($zones as $zone) 
         {
-            $choices[$val] = $val;
+            $zone = explode('/', $zone); // 0 => Continent, 1 => City
+            
+            // Only use "friendly" continent names
+            if ($zone[0] == 'Africa' || $zone[0] == 'America' || $zone[0] == 'Antarctica' || $zone[0] == 'Arctic' || $zone[0] == 'Asia' || $zone[0] == 'Atlantic' || $zone[0] == 'Australia' || $zone[0] == 'Europe' || $zone[0] == 'Indian' || $zone[0] == 'Pacific')
+            {        
+                if (isset($zone[1]) != '')
+                {
+                    $choices[$zone[0]][$zone[0]. '/' . $zone[1]] = str_replace('_', ' ', $zone[1]); // Creates array(DateTimeZone => 'Friendly name')
+                } 
+            }
         }
-
+        
         return form_dropdown($this->form_slug, $choices, $this->value ?: $this->getParameter('default_value'));
+
     }
 }
